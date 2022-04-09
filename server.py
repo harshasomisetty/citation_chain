8e8c506a886a73fffab4bebc823ffc578d26d21f
Harsha Somisetty <harshasomisetty7@gmail.com>
2022-04-09 12:22:15 -0400

    finished basic flask ui for navigating directory metrics

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
 
 
 # List of metadata files to ignore.
-ignore = [".DS_Store"]
+ignore = [".DS_Store", ]
 
 # Data directory path of stored documents
 dir = "data1/"
@@ -13,6 +13,11 @@ app = Flask(__name__, static_folder=dir)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/index')
+def index():
+    name = 'Rosalia'
+    return render_template('index.html', title='Welcome', username=name)
 
 @app.route('/dir')
 @app.route('/dir/')
@@ -26,19 +31,20 @@ def access_path(path_user=""):
         if os.path.isfile(path):
             with open(path, "r") as f:
                 content = f.read()
-            return content
+            return render_template('content.html', title='Content', content = content)
         else:
 
             subdirs = []
             subfiles = []
             for file in os.listdir(path):
-                d = os.path.join(path, file)
-                if os.path.isdir(d):
-                    subdirs.append(file)
-                else:
-                    subfiles.append(file)
+                if file not in ignore:
+                    d = os.path.join(path, file)
+                    if os.path.isdir(d):
+                        subdirs.append(file)
+                    else:
+                        subfiles.append(file)
 
-            return " ".join(subdirs)
+            return render_template('directory.html', title='Content', subdirs = sorted(subdirs), subfiles = sorted(subfiles), path = "/dir/"+path_user)
             
         
     else:
@@ -47,3 +53,5 @@ def access_path(path_user=""):
 
 if __name__ == '__main__':
   app.run(debug =True)
+
+
